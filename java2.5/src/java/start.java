acafa4e787db0f07795183229a7680f2de527c15
Koli <zeal23310183@fsgcl.jp>
2024-05-14 10:54:56 +0900

    3.1

--- java2.5/src/java/start.java
+++
@@ -1,8 +0,0 @@
-package java;
-
-public class start {
-	public static void main(String[]args)
-	{
-		System.out.println("こんばんは");
-	}
-}
---
+++ java2.5/src/start.java
@@ -0,0 +1,12 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    System.out.println("プログラムを美行します。");
+    MyFrame framel =new RectTest();
+     
+    		 
+	}
+
+}

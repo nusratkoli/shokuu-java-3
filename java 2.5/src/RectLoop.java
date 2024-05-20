7e93c133c88869450a0f1675f056ea1fac0ec385
Koli <zeal23310183@fsgcl.jp>
2024-05-20 11:53:54 +0900

    practice 4.3

--- java 2.5/src/RectLoop.java
+++ java 2.5/src/RectLoop.java
@@ -7,7 +7,7 @@ public class RectLoop extends MyFrame {
 			int x = 30;
 			
 		
-		for( int i= 0 ; i< 15; i++)
+		for( int i= 0 ; i< 10; i++)
 			{
 			System.out.println(x);
 			fillRect(x,80,10,100);

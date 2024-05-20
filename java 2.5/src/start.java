a4f7f4abceeedc2b3452a8ba72da5df2d933d1c4
Koli <zeal23310183@fsgcl.jp>
2024-05-20 11:06:52 +0900

    4-2

--- java 2.5/src/start.java
+++ java 2.5/src/start.java
@@ -4,7 +4,7 @@ public class start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
     System.out.println("プログラムを美行します");
-    MyFrame framel =new ManyFigures();
+    MyFrame framel =new RectLoop();
      
     		 
 	}

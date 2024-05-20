8f19dd351290ef5675d32fac5de399160d4f61da
Koli <zeal23310183@fsgcl.jp>
2024-05-20 10:35:38 +0900

    3.5 page 60

--- java 2.5/src/EectAndOval.java
+++ java 2.5/src/EectAndOval.java
@@ -1,10 +1,10 @@
 
 public class EectAndOval extends MyFrame{
 	public void run (){
-		fillRect(40,20,100,100);
+		fillRect(100,100,40,20);
 		fillRect(80,120,80,20);
-		fillOval(90,130,20,20);
-		fillOval(110,130,20,20);
+		fillOval(90,140,20,20);
+		fillOval(130,130,20,20);
 		
 	
 	}

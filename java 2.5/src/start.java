450ba34e416d7859ff84541f6e7f9a75f9e475b9
Koli <zeal23310183@fsgcl.jp>
2024-05-20 10:11:12 +0900

    3.5

--- java 2.5/src/start.java
+++ java 2.5/src/start.java
@@ -4,8 +4,8 @@ public class start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
     System.out.println("プログラムを美行します");
-    MyFrame framel =new ovaltest();
-     
+    MyFrame framel =new TextTest();
+     d
     		 
 	}
 

3e3310bf096df675230672a684d3302273e95250
Koli <zeal23310183@fsgcl.jp>
2024-05-21 11:54:41 +0900

    5.1

--- java 2.5/src/Practicej.java
+++ java 2.5/src/Practicej.java
@@ -2,22 +2,31 @@
 public class Practicej extends MyFrame{
 	public void run()
 	{
+//		setColor(0,128,0);
+//	
+//		int x =0;
+//		
+//		int h = 20;
+//		
+//		for (int i=0; i<8; i++)
+//			
+//		{
+//			System.out.println(x);
+//			fillRect(x,x,h,h);
+//			x+=h;
+//			h+=10;
+//			
+//			
+//		}
+		
 		setColor(0,128,0);
-	
-		int x =0;
-		
-		int h = 20;
-		
+		int x = 0;
+		int h =20;
 		for (int i=0; i<8; i++)
-			
 		{
-			System.out.println(x);
-			fillRect(x,x,h,h);
-			x+=h;
-			h+=10;
-			
 			
 		}
+		
 		
 		
 	}

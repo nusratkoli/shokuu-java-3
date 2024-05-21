e6a5c6513dad1c9a93862c3c831a28068bf4a08c
Koli <zeal23310183@fsgcl.jp>
2024-05-21 11:47:08 +0900

    practice

---
+++ java 2.5/src/Practice.java
@@ -0,0 +1,28 @@
+
+public class Practice extends MyFrame{
+	public void run()
+	{
+		setColor(0,128,0);
+		
+		int x =30;
+		int y = 40;
+		while (x<190)
+		
+			fillRect(x,y,40,80);
+			fillRect(x,y,50,100);
+			fillRect(x,y,60,120);
+			fillRect(x,y,70,130);
+			fillRect(x,y,80,140);
+			fillRect(x,y,90,150);
+			fillRect(x,y,100,160);
+			fillRect(x,y,110,180);
+			fillRect(x,y,120,190);
+			x+=20;
+			y+=20;
+	}
+	     
+	
+		
+		
+
+}
---
+++ java 2.5/src/Practicej.java
@@ -0,0 +1,25 @@
+
+public class Practicej extends MyFrame{
+	public void run()
+	{
+		setColor(0,128,0);
+	
+		int x =0;
+		
+		int h = 20;
+		
+		for (int i=0; i<8; i++)
+			
+		{
+			System.out.println(x);
+			fillRect(x,x,h,h);
+			x+=h;
+			h+=10;
+			
+			
+		}
+		
+		
+	}
+
+}

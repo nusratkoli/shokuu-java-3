db477dd139c789bf384235ba9846a75ffb160874
Koli <zeal23310183@fsgcl.jp>
2024-05-27 11:50:22 +0900

    practice5.3 page 100

--- java 2.5/src/Animation.java
+++ java 2.5/src/Animation.java
@@ -3,38 +3,34 @@ public class Animation extends MyFrame {
 	public void run() {
 		int y = 100;
 		int x = 30;
-		while (y <= 200) {
+		for(int i = 0; i< 3; i++) {
+		while(x<=170){
 			//1
 			clear();
 			//2
 			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100);
-			y += 5;
+			System.out.println(x);
 			x += 5;
 
 			sleep(0.1);
 		}
-		while (x >= 30) {
+		while(x>=30) {
+		 
 			//1
 			clear();
 			//2
 			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100);
+			System.out.println(x);
 			x -= 5;
 
 			sleep(0.1);
+		
 		}
-		while (y>= 30) {
-			//1
-			clear();
-			//2
-			setColor(0, 128, 0);
-			fillRect(x, y, 10, 100);
-			y -= 5;
-			x+=5;
-			sleep(0.1);
-
 		}
+		
+		
 
 	}
 

eb61bedd446a01aaea5e550bde1c36845c15ec6a
Koli <zeal23310183@fsgcl.jp>
2024-05-27 12:05:02 +0900

    おまけ問題その１

--- java 2.5/src/Animation.java
+++ java 2.5/src/Animation.java
@@ -3,7 +3,7 @@ public class Animation extends MyFrame {
 	public void run() {
 		int y = 100;
 		int x = 30;
-		for(int i = 0; i< 3; i++) {
+		
 		while(x<=170){
 			//1
 			clear();
@@ -15,7 +15,7 @@ public class Animation extends MyFrame {
 
 			sleep(0.1);
 		}
-		while(x>=30) {
+		while(y<=200) {
 		 
 			//1
 			clear();
@@ -23,12 +23,38 @@ public class Animation extends MyFrame {
 			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100);
 			System.out.println(x);
-			x -= 5;
+			y+= 5;
 
 			sleep(0.1);
 		
+		
 		}
-		}
+		 while (x>30)
+		 {
+			//1
+				clear();
+				//2
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				System.out.println(x);
+				x -= 5;
+
+				sleep(0.1);
+			 
+		 }
+		  while(y>170)
+		  {
+			//1
+				clear();
+				//2
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				System.out.println(x);
+				y+= 5;
+
+				sleep(0.1);
+			
+		  }
 		
 		
 

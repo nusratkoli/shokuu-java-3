7329f2f8b24968a4f8459fac6c2eae2e343c3d80
Koli <zeal23310183@fsgcl.jp>
2024-05-27 10:47:02 +0900

    5-3

--- java 2.5/src/Animation.java
+++ java 2.5/src/Animation.java
@@ -2,16 +2,35 @@
 public class Animation extends MyFrame {
 	public void run () 
 	{
-		int x = 30;
+		int y = 100;
+		int x =30;
+		while (true) {
 		while (x<=170)
 		{
 			//1画面を消す
 			clear();
-			//(2)
+			//(2)四
 			setColor(0,128,0);
-			fillOval(x,80,80,100);
+			fillRect(x,y,10,100);
+			//x and y 
+			
 			x+=5;
+			//y を
 			sleep(0.1);
+		}
+		while (x>=30)
+		{
+			//1画面を消す
+			clear();
+			//(2)四
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			//x and y 
+			
+			x-=5;
+			//y を
+			sleep(0.1);
+		}
 		}
 	}
 

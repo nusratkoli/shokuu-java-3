ad8a5b7fa4ce6f9978f37a10d5e7a0d1865b51ea
Koli <zeal23310183@fsgcl.jp>
2024-05-28 10:20:41 +0900

    6.17 page 116

--- java 2.5/src/DisplayFaceObject.java
+++ java 2.5/src/DisplayFaceObject.java
@@ -1,14 +1,27 @@
 
 public class DisplayFaceObject extends MyFrame {
  public void run () {
-	 Face face1=new Face (25,50);
+	
+	 
+	 Face face1=new Face (50,50);
+	 Face face2=new Face (200,100);
+	 for(int i=0; i<30;i++)
+	 {
+		 clear();
+		 
+	 
+		 
 	 face1.draw(this);
-	 
-	 Face face2=new Face (125,150);
 	 face2.draw(this);
+	 face1.x+=5;
+	 face1.y+=5;
 	 
-	 Face face3=new Face (225,50);
-	 face3.draw(this);
+	 face2.x-=5;
+	 face2.y-=5;
+	 sleep(0.1);
+	 
+	 }
  }
+	 
  
 }
